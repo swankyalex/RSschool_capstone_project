6419c7fb82ea88391e2537aaf76b2fb9b84841f6
swankyalex <72443505+swankyalex@users.noreply.github.com>
2022-04-30 18:15:58 +0300

    Added gridsearchCV to random forest and log-regr (#3)

--- src/forest_model/consts.py
+++ src/forest_model/consts.py
@@ -4,7 +4,6 @@ _this_file = Path(__file__).resolve()
 
 DIR_REPO = _this_file.parent.parent.parent.resolve()
 DATA_PATH = (DIR_REPO / "data").resolve()
-DIR_IDEA = (DIR_REPO / ".idea").resolve()
 DIR_SCRIPTS = (DIR_REPO / "scripts").resolve()
 DIR_SRC = (DIR_REPO / "src").resolve()
 DIR_MODEL = (DIR_SRC / "models").resolve()
---
+++ src/forest_model/get_model_and_params.py
@@ -0,0 +1,45 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+
+models = {"log": LogisticRegression, "forest": RandomForestClassifier}
+
+log_params = {
+    "1": {"C": [0.01, 0.1, 1], "fit_intercept": [True, False]},
+    "2": {"C": [0.0001, 0.001, 0.01], "fit_intercept": [True, False]},
+    "3": {"C": [1, 10, 100], "fit_intercept": [True, False]},
+}
+
+forest_params = {
+    "1": {
+        "max_depth": [5, 7, 9],
+        "min_samples_leaf": [2, 5, 7],
+        "min_samples_split": [2, 5, 9],
+    },
+    "2": {
+        "max_depth": [3, 4, 5],
+        "min_samples_leaf": [1, 2, 3],
+        "min_samples_split": [2, 3, 4],
+    },
+    "3": {
+        "max_depth": [8, 9, 10],
+        "min_samples_leaf": [10, 20, 30],
+        "min_samples_split": [10, 15, 20],
+    },
+}
+
+params = {"log": log_params, "forest": forest_params}
+
+
+def get_model(model_name, random_state):
+    assert model_name in models, f"{model_name} is incorrect name!"
+    model = models[model_name]
+    if model_name == "log":
+        model = model(random_state=random_state, max_iter=300, solver="liblinear")
+    elif model_name == "forest":
+        model = model(random_state=random_state, n_estimators=100)
+    return model
+
+
+def get_params(model_name, param_set):
+    parameters = params[model_name][param_set]
+    return parameters
