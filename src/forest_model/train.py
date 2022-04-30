a7f245b028c436359f5ff2c13b8b6d726c4d6122
swankyalex <72443505+swankyalex@users.noreply.github.com>
2022-04-30 15:20:55 +0300

    ML flow implemented (#2)

--- src/forest_model/train.py
+++ src/forest_model/train.py
@@ -2,6 +2,8 @@ import argparse
 import os
 import pickle
 
+import mlflow
+import mlflow.sklearn
 import click
 import numpy as np
 from consts import DATA_PATH, DIR_MODEL
@@ -39,23 +41,31 @@ data_path, model_path, random_state, max_iter = parse_args()
 os.makedirs(os.path.dirname(model_path), exist_ok=True)
 
 
-def train_model(data_path):
+def train_model():
     X, y = get_data(data_path)
-    model = LogisticRegression(
-        solver="liblinear", C=0.01, max_iter=max_iter, random_state=random_state
-    )
     scoring = get_metrics()
-    scores = cross_validate(model, X, y, cv=5, scoring=scoring, n_jobs=-1)
-    accuracy = np.mean(scores["test_accuracy"])
-    roc_auc = np.mean(scores["test_roc_auc"])
-    f1 = np.mean(scores["test_f1"])
-    log_loss = np.mean(scores["test_log_loss"])
-    model.fit(X, y)
-    pickle.dump(model, open(model_path, "wb"))
-    click.echo(f"Model is saved to {model_path}.")
-    click.echo(
-        f"Result accuracy - {accuracy}, roc_auc - {roc_auc}, f1 score - {f1}, log_loss - {log_loss}"
-    )
+    with mlflow.start_run():
+        model = LogisticRegression(
+            solver="liblinear", C=0.01, max_iter=max_iter, random_state=random_state
+        )
+        scores = cross_validate(model, X, y, cv=5, scoring=scoring, n_jobs=-1)
+        accuracy = np.mean(scores["test_accuracy"])
+        roc_auc = np.mean(scores["test_roc_auc"])
+        f1 = np.mean(scores["test_f1"])
+        log_loss = np.mean(scores["test_log_loss"])
+        mlflow.log_param("max_iter", max_iter)
+        mlflow.log_param("Model", "Log_Regression")
+        mlflow.log_metric("accuracy", float(accuracy))
+        mlflow.log_metric("roc_auc", float(roc_auc))
+        mlflow.log_metric("f1", float(f1))
+        mlflow.log_metric("log_loss", float(log_loss))
+        mlflow.sklearn.log_model(model, "model")
+        model.fit(X, y)
+        pickle.dump(model, open(model_path, "wb"))
+        click.echo(f"Model is saved to {model_path}.")
+        click.echo(
+            f"Result accuracy - {accuracy}, roc_auc - {roc_auc}, f1 score - {f1}, log_loss - {log_loss}"
+        )
 
 
-train_model(data_path)
+train_model()
